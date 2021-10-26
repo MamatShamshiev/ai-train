33b3e155b1f4924c8dfc45574276b432d387bee0
Mamat Shamshiev <mamat@diagnocat.com>
2021-10-26 11:58:03 +0000

    flush

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ EXP_NAME=$1
 CONFIG="main.yaml"
 shift 1
 
-PYTHONPATH=$(pwd)/src python -m train --config-file configs/$CONFIG --num-gpus 1 OUTPUT_DIR $(pwd)/outputs/$EXP_NAME "$@"
+PYTHONPATH=$(pwd)/src python -m train --config-file configs/$CONFIG --num-gpus 1 "$@" OUTPUT_DIR $(pwd)/outputs/$EXP_NAME
