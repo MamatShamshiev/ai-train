7e09fe3268856fd095ddde2f31779518c51b4162
Mamat Shamshiev <mamat@diagnocat.com>
2021-11-02 06:45:39 +0000

    mv configs

--- train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-EXP_NAME=$1
-CONFIG=$2
-shift 2
-
-PYTHONPATH=$(pwd)/src python -m train --config-file configs/$CONFIG --num-gpus 1 OUTPUT_DIR $(pwd)/outputs/$EXP_NAME "$@"
---
+++ train_dt2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+EXP_NAME=$1
+CONFIG=$2
+shift 2
+
+PYTHONPATH=$(pwd)/src python -m train --config-file configs/dt2/$CONFIG --num-gpus 1 OUTPUT_DIR $(pwd)/outputs/$EXP_NAME "$@"
